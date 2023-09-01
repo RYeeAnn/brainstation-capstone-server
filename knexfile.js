0ba87495cc70e2b198d0f69ff119ae4995fd1f36
Ryan <ryeean16@gmail.com>
2023-09-01 13:27:20 -0700

    add production config for knex

--- knexfile.js
+++ knexfile.js
@@ -2,19 +2,35 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-  client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    charset: "utf8",
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      charset: "utf8",
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
   },
-  migrations: {
-    directory: './migrations',
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      charset: "utf8",
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
   },
-  seeds: {
-    directory: './seeds',
-  },
-},
 };
