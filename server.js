16877c666ed6fdb0789fc2eaa8fa0eb85ba8aba1
Ryan <ryeean16@gmail.com>
2023-12-05 15:31:11 -0800

    push

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const fs = require('fs');
 const cors = require('cors');
-const axios = require('axios');
 const app = express();
+const troubleshootRouter = require('./routes/troubleshootRouter');
 const AWS = require('aws-sdk');
 
 require('dotenv').config();
@@ -20,6 +21,7 @@ const lexruntime = new AWS.LexRuntimeV2();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/troubleshootPage', troubleshootRouter);
 
 app.post('/lex', (req, res) => {
   const params = {
