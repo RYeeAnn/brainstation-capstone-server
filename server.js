12332b1bb56012a4679f69e89f4293a53cccd7d1
Ryan <ryeean16@gmail.com>
2023-12-04 17:15:55 -0800

    post to lex

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
-const fs = require('fs');
-const troubleshootRouter = require('./routes/troubleshootRouter');
 const AWS = require('aws-sdk');
 
 require('dotenv').config();
@@ -10,55 +9,48 @@ require('dotenv').config();
 const PORT = process.env.PORT || 5050;
 
 AWS.config.update({
-    region: 'us-east-1', // Your AWS region
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Load from environment variable
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // Load from environment variable
-  });
+  region: 'us-east-1', // Your AWS region
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Load from environment variable
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // Load from environment variable
+});
 
-const lexruntime = new AWS.LexRuntimeV2(); // Make sure to use LexRuntimeV2 for Amazon Lex V2
+const lexRuntimeEndpoint = 'https://runtime.lex.us-east-1.amazonaws.com'; // Change the region accordingly
+const lexruntime = new AWS.LexRuntimeV2();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'))
-app.use('/troubleshootPage', troubleshootRouter);
+app.use(express.static('public'));
 
+app.post('/lex', (req, res) => {
+  const params = {
+    botId: "IBSYCU6CU5",
+    botAliasId: "TSTALIASID",
+    localeId: "en_US",
+    sessionId: req.body.userId,
+    text: req.body.inputText,
+  };
 
+  const lexRequestURL = `${lexRuntimeEndpoint}/bot/${params.botId}/alias/${params.botAliasId}/user/${params.sessionId}/text`;
 
-app.post('https://cruisin-df8cc0f5b5d6.herokuapp.com/', (req, res) => {
-    const params = {
-      botId: "IBSYCU6CU5", // Your bot ID
-      botAliasId: "TSTALIASID", // The bot alias ID from the AWS CLI output
-      localeId: "en_US", // Your locale ID
-      sessionId: req.body.userId, // Unique session identifier for the conversation
-      text: req.body.inputText, // User input text
-    };
-  
-    lexruntime.recognizeText(params, function (err, data) {
-      if (err) {
-        console.error('AWS Lex Error:', err);
-        res.status(500).json({
-          message: 'Error processing your message',
-          error: err.message,
-        });
-      } else {
-        // Check if Lex returned a message
-        if (data.messages && data.messages.length > 0) {
-          res.json({ message: data.messages[0].content });
-        } else {
-          // Handle no message response
-          res.json({ message: "Sorry, I didn't understand that." });
-        }
-      }
+  axios.post(lexRequestURL, { inputText: params.text })
+    .then(response => {
+      res.json({ message: response.data.message });
+    })
+    .catch(error => {
+      console.error('AWS Lex Error:', error);
+      res.status(500).json({
+        message: 'Error processing your message',
+        error: error.message,
+      });
     });
 });
 
 app.get("/", (req, res) => {
-    console.log("Made it to the server!")
-    res.send("Welcome to the troubleshoot server!")
+  console.log("Made it to the server!")
+  res.send("Welcome to the troubleshoot server!")
 });
 
-app.listen(process.env.PORT || 5050, () => {
-  console.log(`Express app running on port: ${process.env.PORT || 5050}`);
+app.listen(PORT, () => {
+  console.log(`Express app running on port: ${PORT}`);
 });
-
